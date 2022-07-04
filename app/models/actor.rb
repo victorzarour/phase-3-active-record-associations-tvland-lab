e683a5e07502bcd14a741fd523dd5759a86e786b
Victor <vx_zz@hotmail.com>
2022-07-03 20:10:59 -0400

    commit tree 441db32dc173022f08df5aea1439cc173ee96458

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        characters.map do |c|
+            "#{c.name} - #{c.show.name}"
+        end
+        # binding.pry
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        actors.map do |a|
+            "#{a.first_name} #{a.last_name}"
+        end
+        # binding.pry
+    end
 end
